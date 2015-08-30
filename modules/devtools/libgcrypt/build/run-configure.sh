2205885387de81b54e0e6745b1755ee032af2b6b
uraymeiviar <uraymeiviar@gmail.com>
2015-08-30 23:01:37 +0700

    added libgcrypt

---
+++ modules/devtools/libgcrypt/build/run-configure.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+./../libgcrypt-1.6.3/configure \
+--prefix=/usr
---
+++ modules/devtools/libgcrypt/download-src.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+wget https://gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-1.6.3.tar.bz2
+tar -jxvf ./libgcrypt-1.6.3.tar.bz2
