ef04ecd8f915a704d4f873ee1d9bbb7e4f8459ea
uraymeiviar <uraymeiviar@gmail.com>
2015-08-31 03:40:56 +0700

    added mpfr

---
+++ modules/devtools/mpfr/build/run-configure.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./../mpfr-3.1.3/configure --prefix=/usr --enable-thread-safe
---
+++ modules/devtools/mpfr/download-src.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+wget http://www.mpfr.org/mpfr-current/mpfr-3.1.3.tar.xz
+tar -Jxvf ./mpfr-3.1.3.tar.xz
+
